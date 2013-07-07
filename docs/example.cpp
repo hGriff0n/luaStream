1c4684681f624338b318e8eaf08f1812275f1f45
hGriff0n <ghooper96@gmail.com>
2013-07-07 12:42:57 -0400

    docs_1
    Added "example.cpp" and "todo.txt"
    Added text to "api overview.txt"

---
+++ docs/example.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <iomanip>
+
+#include "luaBindings.h"
+#include "luaStream.h"
+#include "luaManip.h"
+#include "luaDebug.h"
+
+int main(int argc,const char* argv[]) {
+	using namespace std;
+
+	lua::luastream str;
+	bool test = true;
+	try {
+		str << 5 << 4 << 3 << 2 << 1 << lua::pack(3) << lua::unpack();
+	}
+	catch (lua::error& e) {
+		cout << e.what();
+		cin.get(); exit(0);
+	};
+
+	lua::debugStack(cout,*str);
+	cin.get();
+};
+
+void lua::luaState::openState(lua_State* L) {
+	luaL_openlibs(L);
+};
